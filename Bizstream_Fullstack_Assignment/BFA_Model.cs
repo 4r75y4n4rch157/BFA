9cff08882c5c2dd72de70fd47ab4ebaafa519652
4r75y4n4rch157 <44687782+4r75y4n4rch157@users.noreply.github.com>
2022-03-21 17:24:21 -0400

    Add files via upload

---
+++ Bizstream_Fullstack_Assignment/BFA_Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactUS.ViewModel
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string First_Name { get; set; }
+        [Required]
+        public string Last_Name{ get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
